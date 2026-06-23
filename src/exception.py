bdcf6a9cc0fb17942cbf98e33ce1af7cab2c4bfe
Krishna Kaushik <vkrishnakaushik@gmail.com>
2026-06-23 16:23:59 +0530

    modified logs and exceptions

--- src/exception.py
+++ src/exception.py
@@ -24,12 +24,7 @@ class CustomException(Exception):
         return self.error_message 
     
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Division by zero error")
-        raise CustomException(e,sys)
+
         
 
 
