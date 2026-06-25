c65b7cf4b4bcedf894de8232bfcdeb10e36ffe21
Krishna Kaushik <vkrishnakaushik@gmail.com>
2026-06-25 11:26:08 +0530

    Hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -46,7 +46,62 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params = {
+
+                "Random Forest": {
+                    "n_estimators": [100, 200, 300],
+                    "max_depth": [None, 10, 20, 30],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4],
+                    "max_features": ["sqrt", "log2"]
+                },
+
+                "Decision Tree": {
+                    "criterion": ["squared_error", "poisson", "absolute_error"],
+                    "splitter": ["best", "random"],
+                    "max_depth": [None, 5, 10, 20],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4]
+                },
+
+                "Gradient Boosting": {
+                    "n_estimators": [100, 200],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "max_depth": [3, 5, 7],
+                    "subsample": [0.8, 1.0],
+                    "min_samples_split": [2, 5],
+                    "min_samples_leaf": [1, 2]
+                },
+
+                "Linear Regression": {
+                    "fit_intercept": [True, False],
+                    "positive": [True, False]
+                },
+
+                "XGBRegressor": {
+                    "n_estimators": [100, 200, 300],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "max_depth": [3, 5, 7],
+                    "subsample": [0.8, 1.0],
+                    "colsample_bytree": [0.8, 1.0],
+                    "gamma": [0, 0.1, 0.3]
+                },
+
+                "CatBoosting Regressor": {
+                    "iterations": [100, 200, 300],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "depth": [4, 6, 8],
+                    "l2_leaf_reg": [1, 3, 5]
+                },
+
+                "AdaBoost Regressor": {
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.01, 0.1, 1.0],
+                    "loss": ["linear", "square", "exponential"]
+                }
+            }
+
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             ## To get best model score from model_report dict
             best_model_score = max(sorted(model_report.values()))
