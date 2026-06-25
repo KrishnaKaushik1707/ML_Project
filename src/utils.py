5804198371748acaef9e5597c154000da8587c2a
Krishna Kaushik <vkrishnakaushik@gmail.com>
2026-06-25 16:20:14 +0530

    Created UI

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,6 @@ import sys
 import numpy as np
 import pandas as pd
 import pickle
-import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
@@ -52,6 +51,14 @@ def evaluate_model(X_train,y_train,X_test,y_test,models,param):
 
         return report
 
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
     
